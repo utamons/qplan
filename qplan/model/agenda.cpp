767df3cadd78166f7a7ec64bf9ab05f6afb2fee1
Oleg Zaidullin <cornknight@gmail.com>
2022-04-23 13:42:47 +0600

    Getters/setters for Agenda

--- qplan/model/agenda.cpp
+++ qplan/model/agenda.cpp
@@ -4,3 +4,13 @@ Agenda::Agenda()
 {
 
 }
+
+const std::vector<List> &Agenda::getLists() const
+{
+    return lists;
+}
+
+void Agenda::setLists(const std::vector<List> &newLists)
+{
+    lists = newLists;
+}
